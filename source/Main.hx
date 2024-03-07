556c67aaf797d71ce7f9a3f6743ac145f72fbc5f
Random people <98900217+justaguy6@users.noreply.github.com>
2024-03-07 20:02:28 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -77,7 +77,7 @@ class Main extends Sprite
 		// the reason for this is we're going to be handling our own cache smartly
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		#if android
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -95,7 +95,7 @@ class Main extends Sprite
 
 	public static function dumpCache()
 	{
-		///* SPECIAL THANKS TO HAYA
+		///* SPECIAL THANKS TO JOSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 		@:privateAccess
 		for (key in FlxG.bitmap._cache.keys())
 		{
