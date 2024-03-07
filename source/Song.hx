45e3268c845b50e8d5a7576ec7bcfffea0fbe4f7
Random people <98900217+justaguy6@users.noreply.github.com>
2024-03-07 19:40:39 -0300

    Update Song.hx

--- source/Song.hx
+++ source/Song.hx
@@ -103,7 +103,7 @@ class Song
 		#end
 
 		if(rawJson == null) {
-			#if sys
+			#if desktop //ata, em MODS_ALLOWED n precisa pq ja tem a if em project.xml
 			rawJson = File.getContent(Paths.json(formattedFolder + '/' + formattedSong)).trim();
 			#else
 			rawJson = Assets.getText(Paths.json(formattedFolder + '/' + formattedSong)).trim();
