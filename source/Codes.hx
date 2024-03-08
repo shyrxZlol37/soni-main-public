408f70f6ccbd014ebfe1e8b8d495d56229387587
Random people <98900217+justaguy6@users.noreply.github.com>
2024-03-07 21:15:59 -0300

    a

--- source/Codes.hx
+++ source/Codes.hx
@@ -152,7 +152,8 @@ class Codes extends MusicBeatState
                     FlxG.mouse.visible = false;
                     FlxG.sound.pause();
                     canSelect = false;
-	//	   (new FlxVideo(Paths.video('Peter Griffin in City Escape'))).finishCallback = function() {
+	//	    (new FlxVideo(Paths.video('Peter Griffin in City Escape'))).finishCallback = function() 
+		    {
 
                         FlxG.sound.resume();
                         FlxG.mouse.visible = true;
